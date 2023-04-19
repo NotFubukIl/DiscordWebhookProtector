f3bef72312c3fb4d1a55b49f71a23e4838768f86
Not Bouki <pro@notfubuki.xyz>
2023-04-19 18:41:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    webhook: "https://discord.com/api/webhooks/1085996307673514104/391oMv1r8b3Rhk_O-1J5Rf05r-Kys80omjRlhMFCNwyOfh2brRKcxVrFsuYwovf-cXfb",
+    webhook: "YOUR_WEBHOOK",
     port: 3000
 }
