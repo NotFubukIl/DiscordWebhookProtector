02c89047f7abb1c378c37ff0aab3204179803434
Not Bouki <pro@notfubuki.xyz>
2023-07-09 21:40:47 +0200

    Update index.js

--- endpoints/get/index.js
+++ endpoints/get/index.js
@@ -1,7 +1,7 @@
 module.exports = async function(req, res) {
     var infos = await fetch(webhook)
     var json = await infos.json();
-    ["id", "token", "channel_id", "guild_id"].forEach(r => json[r] = `RemProtect_${random()}`);
-    log("GET Request, deleted: " + ["id", "token", "channel_id", "guild_id"].join(", ") + " Values")
+    ["id", "token", "channel_id", "guild_id", "url"].forEach(r => json[r] = `RemProtect_${random()}`);
+    log("GET Request, deleted: " + ["id", "token", "channel_id", "guild_id", "url"].join(", ") + " Values")
     res.json(json)
 }
